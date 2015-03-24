44c6964c5a37b750e4bc49348e01d069fb803188
qmliu <qmliu@pearlinpalm.com>
2015-03-24 16:37:32 +0800

    upgrade cur_page? methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def cur_page?(*args)
-    _con_name,_action = args
+    _con_name,*_action = args
     _con_name = controller_name if _con_name.blank?
-    if _action.nil? && _con_name == controller_name
+    if _action.blank? && _con_name == controller_name
       return true
-    elsif _action == action_name && _con_name == controller_name
+    elsif _action.include?(action_name) && _con_name == controller_name
       return true
     end
   end
