84147c559795ffa7add6ee3eef5d41e9240a380a
qmliu <qmliu@pearlinpalm.com>
2015-03-31 21:22:51 +0800

    fix gb2312 mail error in receive job

--- app/jobs/receive_email_job.rb
+++ app/jobs/receive_email_job.rb
@@ -68,7 +68,9 @@ class ReceiveEmailJob < ActiveJob::Base
       puts part.charset
       next if part.content_type !~ /^text\/html/
       #只解析html格式邮件
-      body =Nokogiri::HTML(part.body.decoded.force_encoding(part.charset),"UTF-8")
+      #binding.pry
+      #2015-03-31 21:21 gb2312时要使用 fonce_encoding 为 gbk
+      body =Nokogiri::HTML(part.body.decoded.force_encoding(part.charset=="utf-8" ? 'utf-8':'gbk'),"UTF-8") rescue Rails.logger.info("编码错误:#{$!.message}")
       #get need fill user
       next if (_need_fills = body.css("tr.need_fill").presence || body.css("tr[name=need_fill]")).blank?
 
