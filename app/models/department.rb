cb41c925a9d11cf6d608016d4017efff4acff6c3
qmliu <qmliu@pearlinpalm.com>
2015-04-10 10:06:30 +0800

    udpate fiels

--- app/models/department.rb
+++ app/models/department.rb
@@ -10,6 +10,9 @@ class Department < ActiveRecord::Base
   #1,可使用假期id,
   #2,用户年度假期所需包含列-需用到Journal::CheckType中的中文描述及id来从journals表中得到汇总]
   #3,需排除check_type
+  #TODO: 所各组所可用的假期放到数据表attend_rules 中进行保存,
+  #每一个规则所具有的假期，年度假期可使用列，及邮件展示时的显示列等
+  #是否可以把holiday,check_types report_titles 给放到一张表中，用特殊标签来区别
   GroupAB = [:group_ab,[1,2,3,4,6,7,8,9,10,11,12,13,14,15,16],[5,11,17],[8,12,22]] #%w{0102 0103} #工作室
   GroupNoSwitchTime = [:group_no_switch_time,[1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17],[5,11,17,8],[9,21,12,22]] #%w{0104 0105 0106} #市场及营销
   GroupSwitchTime =   [:group_switch_time,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17],[5,11,17,12],[9,21,22]]#非工作室
