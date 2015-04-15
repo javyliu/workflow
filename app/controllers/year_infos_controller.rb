9275820783ae503b017b6d9de59c11b289655125
qmliu <qmliu@pearlinpalm.com>
2015-04-15 17:37:06 +0800

    add export the remain holiday

--- app/controllers/year_infos_controller.rb
+++ app/controllers/year_infos_controller.rb
@@ -24,13 +24,18 @@ class YearInfosController < ApplicationController
       end
 
       format.xls do
-        _select = "year,user_id,user_name,name dept_name,year_holiday,sick_leave,affair_leave,switch_leave,ab_point"
+        _select = "year,user_id,user_name,name dept_name,year_holiday,sick_leave,affair_leave,switch_leave,ab_point,null r_year_holiday,null r_sick_leave,null r_affair_leave,null r_switch_leave,null r_switch_leave,null r_ab_point"
         params.permit!
         con_hash,like_hash = construct_condition(:user,like_ary: [:user_name,:email])
         _user_ids = User.where(con_hash).where(like_hash).pluck(:uid) if con_hash || like_hash
         @year_infos = @year_infos.select(_select).joins(" inner join users on user_id = uid inner join departments on dept_code = code")
         @year_infos = @year_infos.where(params[:date]) if params[:date] && params[:date][:year].present?
-        @year_infos = @year_infos.where(user_id: _user_ids) if _user_ids
+        @user_year_journals = Journal.where(["update_date > ?",OaConfig.setting(:end_year_time)]).select("id,user_id,check_type,sum(dval) dval").group(:check_type)
+
+        if _user_ids
+          @year_infos = @year_infos.where(user_id: _user_ids)
+          @user_year_journals = @user_year_journals.where(user_id: _user_ids)
+        end
 
         xsl_file = @year_infos.to_csv(select: _select) do |item,cols|
           _attrs = item.attributes
@@ -39,6 +44,12 @@ class YearInfosController < ApplicationController
           _attrs["affair_leave"] = item.affair_leave.to_f/10
           _attrs["switch_leave"] = item.switch_leave.to_f/10
           _attrs["ab_point"] = item.ab_point.to_f/10
+          #TODO need fix the number type
+          _attrs["r_year_holiday"] = ( item.year_holiday - year_journal(item.user_id,5) ).to_f/10
+          _attrs["r_sick_leave"] = (item.sick_leave - year_journal(item.user_id,17)).to_f/10
+          _attrs["r_affair_leave"] = ( item.affair_leave - year_journal(item.user_id,11) ).to_f/10
+          _attrs["r_switch_leave"] =(item.switch_leave + year_journal(item.user_id,8) + year_journal(item.user_id,12)).to_f/10
+          _attrs["r_ab_point"] =  (item.ab_point + year_journal(item.user_id,9) + year_journal(item.user_id,21)).to_f/10
           _attrs.values_at(*cols)#.tap{|t|Rails.logger.info(t.inspect)}
         end
         #xsl_file
@@ -151,5 +162,10 @@ class YearInfosController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def year_info_params
       params.require(:year_info).permit(:year, :user_id, :year_holiday, :sick_leave, :affair_leave, :switch_leave, :ab_point)
+    end
+
+
+    def year_journal(user_id,check_type_id)
+      @user_year_journals.detect { |e| e.user_id == user_id && e.check_type == check_type_id }.try(:dval).to_i
     end
 end
