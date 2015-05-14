c645643ca1aa8fdb498fea1d7dc34395e011f2a1
qmliu <qmliu@pearlinpalm.com>
2015-05-14 09:53:30 +0800

    update files

--- app/models/chares_database/tblemployee.rb
+++ app/models/chares_database/tblemployee.rb
@@ -9,7 +9,7 @@ module CharesDatabase
     def self.sys_users(need_change_pwd=true,path="/root/sh/mailpasswd.txt")
       pwds = Hash[*YAML.load_file(path).split(/:|\s+/)]
       _date = Date.today
-      self.find_each do |item|
+      self.where("expireDate >= current_date()").find_each do |item|
         #User.create!(uid: item.userId,user_name:item.name,email:item.email,department:item.department,expire_date:item.expireDate,dept_code:item.deptCode,mgr_code:item.mgrCode,title: item.title,onboard_date: item.onboardDate,regular_date: item.regularDate,password: '123123')
         u = User.find_or_initialize_by(uid: item.userId)
         u.user_name = item.name
@@ -45,6 +45,8 @@ module CharesDatabase
         #puts u.inspect
         u.save!
       end
+
+      User.where("expire_date < current_date() ").delete_all
     end
 
     #在users表中的直属管理者,指定经理等人的上一级管理者
