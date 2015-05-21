0e2f46ab6da4df50610a930e03a22d1593b0b950
qmliu <qmliu@pearlinpalm.com>
2015-05-21 11:28:59 +0800

    fix user sys

--- app/models/chares_database/tblemployee.rb
+++ app/models/chares_database/tblemployee.rb
@@ -9,7 +9,7 @@ module CharesDatabase
     def self.sys_users(need_change_pwd=true,path="/root/sh/mailpasswd.txt")
       pwds = Hash[*YAML.load_file(path).split(/:|\s+/)]
       _date = Date.today
-      self.where("expireDate >= current_date()").find_each do |item|
+      self.where("expireDate is null or expireDate >= current_date()").find_each do |item|
         #User.create!(uid: item.userId,user_name:item.name,email:item.email,department:item.department,expire_date:item.expireDate,dept_code:item.deptCode,mgr_code:item.mgrCode,title: item.title,onboard_date: item.onboardDate,regular_date: item.regularDate,password: '123123')
         u = User.find_or_initialize_by(uid: item.userId)
         u.user_name = item.name
