fda551c62f3550982fda995fe82c809abd1fbc52
qmliu <qmliu@pearlinpalm.com>
2015-11-12 18:07:37 +0800

    add uniqueness validate to episodes for the scope: user_id,ck_type,start_date and end_date

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -21,19 +21,21 @@ class Episode < ActiveRecord::Base
   belongs_to :user
   has_many :approves,dependent: :delete_all
 
-  validates :user_id, presence: true
-  validates :comment, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :title, presence: true
-  validates :total_time, presence: true
+  validates :user_id,:comment,:start_date,:end_date,:title,:total_time, presence: true
+  #validates_uniqueness_of :ck_type, conditions: -> {|args| where("? between start_date and end_date", Time.now.to_date )}
 
   has_many :children, class_name: 'Episode',foreign_key: :parent_id,dependent: :delete_all
 
   accepts_nested_attributes_for :children,:allow_destroy => true, :reject_if => :all_blank
 
   #belongs_to :parent,class_name: 'Episode',foreign_key: :parent_id
-  before_save :init_attr
+  before_validation :init_attr
+
+  validate do
+    is_exist_con = Episode.where(["user_id = ? and ck_type = ? and  start_date <= ?  and end_date > ?",self.user_id,self.ck_type,self.start_date.end_of_day.to_s(:db),self.start_date.to_date])
+    is_exist_con = is_exist_con.where("id != ?",self.id) unless self.new_record?
+    errors.add(:base, '已经存在同类申请了，请删除后再次申请！') if is_exist_con.exists?
+  end
 
   #get the parent episode
   #if no parent return self
@@ -61,7 +63,7 @@ class Episode < ActiveRecord::Base
   end
 
   def init_attr
-    self.ck_type = Journal::CheckType.detect{|item|item[6] == self.holiday_id}.try(:second)
+    self.ck_type ||= Journal::CheckType.detect{|item|item[6] == self.holiday_id}.try(:second)
   end
 
   def calcucate_day_total(episode)
