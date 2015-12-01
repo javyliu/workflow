b0715ca2ca2b871fc6181f21ff17f54c4bd3cb7b
qmliu <qmliu@pearlinpalm.com>
2015-12-01 15:21:26 +0800

    admin can update department's mgr_code

--- app/models/chares_database/tbldepartment.rb
+++ app/models/chares_database/tbldepartment.rb
@@ -2,7 +2,8 @@ module CharesDatabase
   #2015-03-26 17:54 javy 在正式库中添加两个表
   class Tbldepartment < ActiveRecord::Base
     self.table_name = 'tbldepartment'
-    self.primary_key = 'mgrCode'
+    #self.primary_key = 'mgrCode'
+    self.primary_key = 'deptCode'
 
     def self.which_rule(old_rule)
       if old_rule.deptCode.start_with?("0108") and old_rule.deptCode != "010899" #平台用固定工作时间
@@ -24,7 +25,9 @@ module CharesDatabase
       CharesDatabase::Tbldepartment.find_each do |item|
         next if item.deptCode.end_with?("99")
         _dept = Department.find_or_initialize_by(code:item.deptCode)
-        _dept.assign_attributes(name:item.deptName,mgr_code:item.mgrCode)
+        _dept.name = item.deptName
+        _dept.mgr_code = item.mgrCode if _dept.new_record?
+        #_dept.assign_attributes(name:item.deptName,mgr_code:item.mgrCode)
         _dept.save!
       end
     end
