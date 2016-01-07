ac5311a237b28b83527fbc47e090f87d88286f44
qmliu <qmliu@pearlinpalm.com>
2016-01-07 15:25:20 +0800

    update files

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,7 +73,7 @@ class User < ActiveRecord::Base
                      end
   end
 
-  #[:year, :user_id, :year_holiday, :sick_leave, :affair_leave, :switch_leave, :ab_point]
+  #[:id,:year, :user_id, :year_holiday, :sick_leave, :affair_leave, :switch_leave, :ab_point,:created_at,:updated_at]
   GroupYearInfoColumns = YearInfo.attribute_names.map(&:intern).slice(3..-3)
   def last_year_info
     @last_year_info ||= self.year_infos.inject do |r,item|
