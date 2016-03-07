6b29f2d1e2831d646fdd0d5490af7390ec067de0
qmliu <qmliu@pearlinpalm.com>
2016-03-07 14:59:08 +0800

    fix some error and delete some test code

--- db/migrate/20160303051857_add_polymorphic_to_approves.rb
+++ db/migrate/20160303051857_add_polymorphic_to_approves.rb
@@ -6,11 +6,11 @@ class AddPolymorphicToApproves < ActiveRecord::Migration
     add_index :approves, [:approveable_id,:approveable_type], name: :approveable_index
 
     #migrate data
-    Approve.update_sql("update approves set approveable_id = episode_id, approveable_type = 'Episode'")
+    Approve.update_all("approveable_id = episode_id, approveable_type = 'Episode'")
     say "migrate data ok"
 
-    #remove_column :approves, :episode_id
-    #say "remove episode_id"
+    remove_column :approves, :episode_id
+    say "remove episode_id"
   end
 
   def down
