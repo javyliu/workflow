fba3cbb2925bd68223b656ea11947acab0bac4d7
qmliu <qmliu@pearlinpalm.com>
2016-03-16 11:13:52 +0800

    update files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
       format.xls do
         @users = @users.except(:includes).includes(:dept)
         cols = "uid,user_name,email,dept,title,mgr_code,assault,assault_start_date,assault_end_date".split(",")
-        xsl_file = @users.to_csv(select: "ID,名字,邮箱,部门,职位,直属管理员ID,是否突击状态,突击开始时间,突击结否时间") do |item,_|
+        xsl_file = @users.to_csv(select: "ID,名字,邮箱,部门,职位,直属管理员ID,是否突击状态,突击开始时间,突击结束时间") do |item,_|
           _attrs = item.attributes
           _attrs["dept"] = item.dept.try(:name)
           _attrs["assault"] = item.assault_state?(@date)
