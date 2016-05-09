f69c5cb760f2d42f15eafa8ea289b6d83cc1129f
qmliu <qmliu@pearlinpalm.com>
2016-05-09 13:12:56 +0800

    sample with rvm in :path do

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,8 @@ set :output, "~/cron_log.log"
 
 #每天早上5点同步checkinout数据库
 job_type :job, "cd :path && :environment_variable=:environment bin/sidekiq_pusher :task :output"
+#job_type :job, ":environment_variable=:environment rvm in :path do :path/bin/sidekiq_pusher :task :output " #bundle exec ruby :task --silent :output"
+
 #同步用户、用户密码等
 every 1.day,at: '4:30am' do
   job "SysUserJob"
