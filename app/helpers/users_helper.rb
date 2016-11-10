c4060d731986481e16acd7194eb291fc1b9e0e66
qmliu <qmliu@pearlinpalm.com>
2016-11-10 16:34:46 +0800

    update files

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,9 @@ module UsersHelper
     @cache_department ||= User.cache_departments
     @cache_department.rassoc(dept).try(:[],0)
   end
+
+  def manager_users_ary
+    #3为部门管理员，3为数据库固定id,表示具有部门管理员角色的用户
+    @manager_users ||= Role.find(3).users.pluck(:user_name,:uid)
+  end
 end
